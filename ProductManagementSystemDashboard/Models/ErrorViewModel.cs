8de78fda9897be8f34055c31bf7ca4d8b56821ef
Sachin Prabuditha <sachinprabuditha@gmail.com>
2025-01-27 20:29:14 +0530

    Add project files.

---
+++ ProductManagementSystemDashboard/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProductManagementSystemDashboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
